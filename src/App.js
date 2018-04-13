7221bf568ad11e38e2f61304d031962f3ba35df7
chicrock <jungsungrak@gmail.com>
2018-04-13 17:54:22 +0900

    Error boundaries

--- src/App.js
+++ src/App.js
@@ -1,6 +1,24 @@
 import React, { Component, Fragment } from "react";
 import { createPortal } from "react-dom";
 
+class ErrorMaker extends Component {
+    state = {
+        friends: ["jisu", "flynn", "dall", "kneeprayer"],
+    };
+
+    componentDidMount = () => {
+        setTimeout(() => {
+            this.setState({
+                friends: undefined,
+            });
+        }, 2000);
+    };
+    render() {
+        const { friends } = this.state;
+        return friends.map((friend) => ` ${friend} `);
+    }
+}
+
 class Portals extends Component {
     render() {
         return createPortal(<Message />, document.getElementById("touchme"));
@@ -9,13 +27,28 @@ class Portals extends Component {
 
 const Message = () => "Just touched it!";
 
+const ErrorFallback = () => " Sorry something went wrong";
+
 /// Imposible return list of element in component without fragment.
 class ReturnTypes extends Component {
+    state = {
+        hasError: false,
+    };
+    componentDidCatch = (error, info) => {
+        console.log(
+            `catched ${error} the info i have is ${JSON.stringify(info)}`
+        );
+        this.setState({
+            hasError: true,
+        });
+    };
     render() {
+        const { hasError } = this.state;
         return (
             <Fragment>
                 <ReturnString />
                 <Portals />
+                {hasError ? <ErrorFallback /> : <ErrorMaker />}
             </Fragment>
         );
     }
