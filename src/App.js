5a1e22da8b90d3baa5daf5a76bcc8d20ed438b1f
chicrock <jungsungrak@gmail.com>
2018-04-24 19:13:19 +0900

    Modify tab space

--- src/App.js
+++ src/App.js
@@ -5,78 +5,78 @@ const Message = () => "Just touched it!";
 
 const ErrorFallback = () => " Sorry something went wrong";
 
-const BoundaryHOC = (ProtectedComponent) =>
-    class BoundaryHOC extends Component {
-        state = {
-            hasError: false,
-        };
-        componentDidCatch = () => {
-            this.setState({
-                hasError: true,
-            });
-        };
-        render() {
-            const { hasError } = this.state;
-
-            if (hasError) {
-                return <ErrorFallback />;
-            } else {
-                return <ProtectedComponent />;
-            }
-        }
-    };
-
-class ErrorMaker extends Component {
+const BoundaryHOC = ProtectedComponent =>
+  class BoundaryHOC extends Component {
     state = {
-        friends: ["jisu", "flynn", "dall", "kneeprayer"],
+      hasError: false,
     };
-
-    componentDidMount = () => {
-        setTimeout(() => {
-            this.setState({
-                friends: undefined,
-            });
-        }, 2000);
+    componentDidCatch = () => {
+      this.setState({
+        hasError: true,
+      });
     };
     render() {
-        const { friends } = this.state;
-        return friends.map((friend) => ` ${friend} `);
+      const { hasError } = this.state;
+
+      if (hasError) {
+        return <ErrorFallback />;
+      } else {
+        return <ProtectedComponent />;
+      }
     }
+  };
+
+class ErrorMaker extends Component {
+  state = {
+    friends: ["jisu", "flynn", "dall", "kneeprayer"],
+  };
+
+  componentDidMount = () => {
+    setTimeout(() => {
+      this.setState({
+        friends: undefined,
+      });
+    }, 2000);
+  };
+  render() {
+    const { friends } = this.state;
+    return friends.map(friend => ` ${friend} `);
+  }
 }
 
 const PErrorMaker = BoundaryHOC(ErrorMaker);
 
 class Portals extends Component {
-    render() {
-        return createPortal(<Message />, document.getElementById("touchme"));
-    }
+  render() {
+    return createPortal(<Message />, document.getElementById("touchme"));
+  }
 }
 
 const PPortals = BoundaryHOC(Portals);
 
 /// Imposible return list of element in component without fragment.
 class ReturnTypes extends Component {
-    render() {
-        return (
-            <Fragment>
-                <ReturnString />
-                <PPortals />
-                <PErrorMaker />
-            </Fragment>
-        );
-    }
+  render() {
+    return (
+      <Fragment>
+        <ReturnString />
+        <PPortals />
+        <PErrorMaker />
+      </Fragment>
+    );
+  }
 }
 
 class ReturnString extends Component {
-    render() {
-        return "hello world";
-    }
+  render() {
+    return "hello world";
+  }
 }
 
 class App extends Component {
-    render() {
-        return <ReturnTypes />;
-    }
+  render() {
+    return <ReturnTypes />;
+  }
 }
 
 export default BoundaryHOC(App);
